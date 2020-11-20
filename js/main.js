de523f2cb70a5258161cf15da86199189e289845
Elizabeth <whosingsin@gmail.com>
2020-11-20 18:43:42 +0300

    card fixed

--- js/main.js
+++ js/main.js
@@ -39,6 +39,7 @@ const createCards = (data) => {
 
   const pokemonCard = document.createElement('a');
   pokemonCard.className = 'card';
+  pokemonCard.href = "#${name}"
   pokemonCard.id = id;
   // pokemonCard.href = 'card.html';
 
@@ -87,12 +88,12 @@ const createCards = (data) => {
 // }
 
 const createCard = (data) => {
-  console.log(data);
-  const { height, id ,name, stats, types, weight, abilities } = data;
-  // console.log(abilities[0].ability.name);
-  // const abilityURL = abilities[0].ability.url;
-  // const statsURL = stats[0].stat.url;
-  // stats.forEach(stat => console.log(stat.base_stat))
+  // console.log(data);
+  const { height, id ,name, stats, types, weight, abilities, location_area_encounters: location } = data;
+
+  let locationURL = location;
+
+  const abilityURL = abilities[0].ability.url;
 
   cardContain.textContent = '';
   
@@ -102,198 +103,183 @@ const createCard = (data) => {
             <span class="pokemon-id">№0${id<10 ? "0"+id : id}</span>
           </h1>
           <div class="card-block">
+            <img src="https://pokeres.bastionbot.org/images/pokemon/${id}.png" alt="" class="card-left-img">
             <div class="card-left">
-              <img src="https://pokeres.bastionbot.org/images/pokemon/${id}.png" alt="" class="card-left-img">
-              <div class="card-stats">
-                <h3 class="stats-title">Stats</h3>
-                <ul class="stats-links">
-                  <li class="stats-link">
-                    <ul class="stats-column">
-                      <li class="meter" data-value="3"></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                    </ul>
-                    <span class="state">HP</span>
-                  </li>
-                  <li class="stats-link">
-                    <ul class="stats-column">
-                      <li class="meter" data-value="3"></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                    </ul>
-                    <span class="state">Attack</span>
-                  </li>
-                  <li class="stats-link">
-                    <ul class="stats-column">
-                      <li class="meter" data-value="3"></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                    </ul>
-                    <span class="state">Defense</span>
-                  </li>
-                  <li class="stats-link">
-                    <ul class="stats-column">
-                      <li class="meter" data-value="3"></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                    </ul>
-                    <span class="state">Special</br> Attacks</span>
-                  </li>
-                  <li class="stats-link">
-                    <ul class="stats-column">
-                      <li class="meter" data-value="3"></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                    </ul>
-                    <span class="state">Special</br> Defense</span>
-                  </li>
-                  <li class="stats-link">
-                    <ul class="stats-column">
-                      <li class="meter" data-value="3"></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                    </ul>
-                    <span class="state">Speed</span>
-                  </li>
-                </ul>
-              </div>
+            <div class="pokemon-info">
+              <ul>
+                <li class="pokemon-par">Height</li>
+                <li class="pokemon-par">${height}.0 m</li>
+                <li class="pokemon-par">Weight</li>
+                <li class="pokemon-par">${weight}.0 kg</li>
+                <li class="pokemon-par">Gender</li>
+                <li class="pokemon-par">
+                  <span class="pokemon-sex background-color-male">Male</span>
+                  <span class="pokemon-sex background-color-female">Female</span>
+                </li>
+              </ul>
+              <ul>
+                <li class="pokemon-par">Category</li>
+                <li class="pokemon-par">Seed</li>
+                <li class="pokemon-par">Abilities</li>
+                <li class="pokemon-par overgrow">${abilities[0].ability.name.toUpperCase()}</li>
+              </ul>
+              <div class="pokemon-info-close-toggle">?</div>
             </div>
-            <div class="card-right">
-              <div class="toggle-version">
-                <span class="toggle-version-span">Versions:</span>
-                <input type="radio" name="version" id="v1" checked></input>
-                <input type="radio" name="version" id="v2"></input>
-                <p class="pokemon-description version1">
-                  When the bulb on its back grows large, 
-                  it appears to lose the ability to stand on its hind legs.
-                </p>
-                <p class="pokemon-description version2">
-                  Exposure to sunlight adds to its strength. 
-                  Sunlight also makes the bud on its back grow larger.
-                </p>
-              </div>
-              <div class="pokemon-info">
-                <ul>
-                  <li class="pokemon-par">Height</li>
-                  <li class="pokemon-par">${height}.0 m</li>
-                  <li class="pokemon-par">Weight</li>
-                  <li class="pokemon-par">${weight}.0 kg</li>
-                  <li class="pokemon-par">Gender</li>
-                  <li class="pokemon-par">
-                    <span class="pokemon-sex background-color-male">Male</span>
-                    <span class="pokemon-sex background-color-female">Female</span>
-                  </li>
-                </ul>
-                <ul>
-                  <li class="pokemon-par">Category</li>
-                  <li class="pokemon-par">Seed</li>
-                  <li class="pokemon-par">Abilities</li>
-                  <li class="pokemon-par overgrow">${abilities[0].ability.name.toUpperCase()}</li>
-                </ul>
-                <div class="pokemon-info-close-toggle">?</div>
-              </div>
-              <div class="pokemon-info pokemon-info-close">
-                <div class="info-close-btn">X</div>
-                <h2 class="info-close">Ability info</h2>
-                <h3 class="info-ability-title">${abilities[0].ability.name.toUpperCase()}</h3>
-                <p class="info-ability-descr">
-                  Powers up Grass-type moves when the Pokémon's HP is low.
-                </p>
-              </div>
-              <div class="tags">
-                <div class="tags-types">
-                  <div class="tags-title">Types</div>
-                  <div class="card-tags card-page">
-                  </div>
-                </div>
-                <div class="tags-weaks">
-                  <div class="tags-title">Weakness</div>
-                  <div class="card-tags card-page">
-                    <div class="card-tag background-color-fire card-page">Fire</div>
-                    <div class="card-tag background-color-ice card-page">Ice</div>
-                  </div>
-                </div>
-              </div>          
-              </div>
+            <div class="pokemon-info pokemon-info-close">
+              <div class="info-close-btn">X</div>
+              <h2 class="info-close">Ability info</h2>
+              <h3 class="info-ability-title">${abilities[0].ability.name.toUpperCase()}</h3>
+              <p class="info-ability-descr">
+                
+              </p>
             </div>
+            <div class="tags">
+              <div class="tags-types">
+                <div class="tags-title">Types</div>
+                <div class="card-tags card-page">
+                </div>
+              </div>
+              <div class="tags-location">
+                <div class="tags-title">Location</div>
+                <div class="card-tags card-page-location">
+                </div>
+              </div>
+            </div>          
+            </div>
+          </div>
+          <div class="card-right">
+          <div class="card-stats">
+            <h3 class="stats-title">Stats</h3>
+            <ul class="stats-links">
+              <li class="stats-link">
+                <ul class="stats-column">
+                  <li class="meter" data-value="3"></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                </ul>
+                <span class="state">HP</span>
+              </li>
+              <li class="stats-link">
+                <ul class="stats-column">
+                  <li class="meter" data-value="3"></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                </ul>
+                <span class="state">Attack</span>
+              </li>
+              <li class="stats-link">
+                <ul class="stats-column">
+                  <li class="meter" data-value="3"></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                </ul>
+                <span class="state">Defense</span>
+              </li>
+              <li class="stats-link">
+                <ul class="stats-column">
+                  <li class="meter" data-value="3"></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                </ul>
+                <span class="state">Special</br> Attacks</span>
+              </li>
+              <li class="stats-link">
+                <ul class="stats-column">
+                  <li class="meter" data-value="3"></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                </ul>
+                <span class="state">Special</br> Defense</span>
+              </li>
+              <li class="stats-link">
+                <ul class="stats-column">
+                  <li class="meter" data-value="3"></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                </ul>
+                <span class="state">Speed</span>
+              </li>
+            </ul>
+          </div>
+        </div>
         </div>
   `;
   
@@ -301,11 +287,39 @@ const createCard = (data) => {
 
   const cardTags = document.querySelector('.card-tags');
 
-  const meter = document.querySelector('.meter');
-  console.log(meter);
-  const statBas = stats[0].base_stat;
-  console.log(stats[0].base_stat);
-  meter.style.top = stats[0].base_stat + "%";
+  const meter = document.querySelectorAll('.meter');
+
+  meter.forEach((item, index) => {
+    item.style.top = 100 - stats[index].base_stat + '%';
+
+  });
+
+  const getLocation = (data) => {
+
+    const locationName = document.querySelector('.card-page-location');
+
+    if (!data.length) {
+      const locationTag = document.createElement('div');
+      locationTag.className = 'card-tag card-tag-location';
+      locationTag.textContent = 'Этого покемона нельзя поймать';
+      locationName.insertAdjacentElement('beforeend', locationTag);  
+    } else {
+      data.forEach(({ location_area }) => {
+
+        const name = location_area.name;
+
+        const locationTag = document.createElement('div');
+        locationTag.className = 'card-tag card-tag-location';
+        
+        locationTag.textContent = name;
+  
+        locationTag.location = name;
+  
+        locationName.insertAdjacentElement('beforeend', locationTag); 
+    })
+    }  
+    
+  }
 
   types.forEach((item) => {
 
@@ -316,10 +330,34 @@ const createCard = (data) => {
     cardTags.insertAdjacentHTML('afterbegin', tag)
   }); 
 
-  infOpen = document.querySelector('.pokemon-info-close-toggle'),
-  pokemonInfoClose = document.querySelector('.pokemon-info-close'),
-  pokemonInfo = document.querySelector('.pokemon-info'),
-  infoCloseBtn = document.querySelector('.info-close-btn');
+  const getAbility = (data) => {
+    const dataEffects = data.effect_entries;
+
+    const englishElem = dataEffects.find((item) => {
+      if (item.language.name === 'en') {return item};
+    })
+
+    let sliced = englishElem.effect.slice(0,380);
+
+    if (sliced.length < englishElem.effect.length) {
+      sliced += '...';
+    }
+
+    const infoAbilityDescr = document.querySelector('.info-ability-descr')
+
+    infoAbilityDescr.textContent = sliced;
+    
+  }; 
+
+  getData.getAllPokemons(abilityURL, getAbility);
+  getData.getAllPokemons(locationURL, getLocation);  
+
+  
+  const infOpen = document.querySelector('.pokemon-info-close-toggle'),
+    pokemonInfoClose = document.querySelector('.pokemon-info-close'),
+    pokemonInfo = document.querySelector('.pokemon-info'),
+    infoCloseBtn = document.querySelector('.info-close-btn');
+
 
   infOpen.addEventListener('click', () => {
     pokemonInfoClose.classList.add('active');
